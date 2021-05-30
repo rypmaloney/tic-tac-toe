4be6ddca9db03b8948bd3e143ea70b03c6e984b5
Ryan Maloney <rypmaloney@gmail.com>
2021-05-30 11:42:55 -0400

    created winning combos array

--- scripts.js
+++ scripts.js
@@ -3,41 +3,47 @@ const container = document.getElementById('container');
 //Gameboard 
 const gameBoard = [1, 2, 'x', 4, 5, 'o', 7, 8, 9]
 
-
-
+const winningCombos = [[1, 2, 3],
+                       [4, 5, 6],
+                       [7, 8, 9],
+                       [1, 4, 7],
+                       [2, 5, 8],
+                       [3, 6, 9],
+                       [1, 5, 9],
+                       [3, 5, 7]]
 
 //creates grid adds corresponding ID--index +1 
-function displayBoard(){
-    
-resetBoard()
-    
-for (i = 0; i < gameBoard.length; i++) {
-    let gameSquare = document.createElement('div');
-    gameSquare.setAttribute('id', `${i + 1}`);
-    gameSquare.classList.add('box');
-    container.appendChild(gameSquare);
-    gameSquare.addEventListener('click', (e) => takeBox(e))
-    
-    let mark = document.createElement('p');
+function displayBoard() {
+
+    resetBoard()
+
+    for (i = 0; i < gameBoard.length; i++) {
+        let gameSquare = document.createElement('div');
+        gameSquare.setAttribute('id', `${i + 1}`);
+        gameSquare.classList.add('box');
+        container.appendChild(gameSquare);
+        gameSquare.addEventListener('click', (e) => takeBox(e))
+
+        let mark = document.createElement('p');
         mark.classList.add('mark');
         gameSquare.appendChild(mark);
-    
-    if (gameBoard[i] === 'x'){
-        mark.textContent = 'X'
-        
-    } else if (gameBoard[i] === 'o'){
-        mark.textContent = 'O'
+
+        if (gameBoard[i] === 'x') {
+            mark.textContent = 'X'
+
+        } else if (gameBoard[i] === 'o') {
+            mark.textContent = 'O'
+        }
     }
-}
 }
 
 
 
 //Change gameboard array by adding player selection-- change 'x' to some playerMark variable
 
-function takeBox(e){
-gameBoard[e.target.id -1] = 'x';
-displayBoard()
+function takeBox(e) {
+    gameBoard[e.target.id - 1] = 'x';
+    displayBoard()
 }
 
 
@@ -56,9 +62,9 @@ displayBoard()
 
 
 //player factory
-const player = () => {
+const player = (mark, ? ) => {
 
-    
+    /*Need to be assigned a mark - x or o
     /* Needs function to change gameBoard*/
 
 }
