f8ca687120bd1ab286d78940a5013d6ed9c53099
Ryan Maloney <rypmaloney@gmail.com>
2021-05-30 11:36:23 -0400

    Created gameboard with js. Made function for adding marks to game boxes. basic styling.

---
+++ scripts.js
@@ -0,0 +1,64 @@
+const container = document.getElementById('container');
+
+//Gameboard 
+const gameBoard = [1, 2, 'x', 4, 5, 'o', 7, 8, 9]
+
+
+
+
+//creates grid adds corresponding ID--index +1 
+function displayBoard(){
+    
+resetBoard()
+    
+for (i = 0; i < gameBoard.length; i++) {
+    let gameSquare = document.createElement('div');
+    gameSquare.setAttribute('id', `${i + 1}`);
+    gameSquare.classList.add('box');
+    container.appendChild(gameSquare);
+    gameSquare.addEventListener('click', (e) => takeBox(e))
+    
+    let mark = document.createElement('p');
+        mark.classList.add('mark');
+        gameSquare.appendChild(mark);
+    
+    if (gameBoard[i] === 'x'){
+        mark.textContent = 'X'
+        
+    } else if (gameBoard[i] === 'o'){
+        mark.textContent = 'O'
+    }
+}
+}
+
+
+
+//Change gameboard array by adding player selection-- change 'x' to some playerMark variable
+
+function takeBox(e){
+gameBoard[e.target.id -1] = 'x';
+displayBoard()
+}
+
+
+//resets gameboard -- end of game & to display board
+function resetBoard() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+
+displayBoard()
+
+
+
+
+
+//player factory
+const player = () => {
+
+    
+    /* Needs function to change gameBoard*/
+
+}
