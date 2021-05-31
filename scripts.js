4f553c46cf7cb5e8cd37dbcbc0fefc7af7a10ffb
Ryan Maloney <rypmaloney@gmail.com>
2021-05-31 14:33:49 -0400

    Created player factory. Wrote checkForWinner() function.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,20 @@
 const container = document.getElementById('container');
 
-//Gameboard 
-const gameBoard = [1, 2, 'x', 4, 5, 'o', 7, 8, 9]
 
-const winningCombos = [[1, 2, 3],
-                       [4, 5, 6],
-                       [7, 8, 9],
-                       [1, 4, 7],
-                       [2, 5, 8],
-                       [3, 6, 9],
-                       [1, 5, 9],
-                       [3, 5, 7]]
+
+
+//Gameboard 
+const gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+let playerArray = [[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9],
+                   [1, 4, 7],
+                   [2, 5, 8],
+                   [3, 6, 9],
+                   [1, 5, 9],
+                   [3, 5, 7]]
+
 
 //creates grid adds corresponding ID--index +1 
 function displayBoard() {
@@ -22,7 +26,6 @@ function displayBoard() {
         gameSquare.setAttribute('id', `${i + 1}`);
         gameSquare.classList.add('box');
         container.appendChild(gameSquare);
-        gameSquare.addEventListener('click', (e) => takeBox(e))
 
         let mark = document.createElement('p');
         mark.classList.add('mark');
@@ -39,12 +42,7 @@ function displayBoard() {
 
 
 
-//Change gameboard array by adding player selection-- change 'x' to some playerMark variable
 
-function takeBox(e) {
-    gameBoard[e.target.id - 1] = 'x';
-    displayBoard()
-}
 
 
 //resets gameboard -- end of game & to display board
@@ -55,16 +53,53 @@ function resetBoard() {
 }
 
 
-displayBoard()
 
 
+//Player Factory
+const player = (mark) => {
+    const playerTurn = () => {
+        let boxes = document.querySelectorAll(".box");
 
+        for (i = 0; i < boxes.length; i++) {
+            boxes[i].addEventListener('click', (e) => {
+                //adds Eventlistener - adds player.mark to gameboard -> updates board
+                gameBoard[e.target.id - 1] = `${mark}`;
+                displayBoard()
 
+                //replaces all instances of turn position in the player array with player.mark
+                for (i = 0; i < playerArray.length; i++) {
+                    turnPos = playerArray[i].indexOf(parseInt(e.target.id));
+                    playerArray[i].splice(turnPos, 1, `${mark}`)
 
-//player factory
-const player = (mark, ? ) => {
-
-    /*Need to be assigned a mark - x or o
-    /* Needs function to change gameBoard*/
-
+                }
+            })
+        }
+    }
+    return {
+        playerTurn,
+        mark,
+        playerArray
+    }
 }
+
+
+//check for winner
+
+function checkForWinner() {
+    for (i = 0; i < playerArray.length; i++) {
+        if (playerArray[i][0] === 'x' &&
+            playerArray[i][1] === 'x' &&
+            playerArray[i][2] === 'x') {
+            console.log('playerx won!')
+        
+        } else if (playerArray[i][0] === 'o' && 
+                   playerArray[i][1] === 'o' &&
+                   playerArray[i][2] === 'o') {
+            console.log('playero won!')
+        }}
+    }
+
+
+
+
+displayBoard()
