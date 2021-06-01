fb51c3220cb14413499be398278c6cf80c68784d
Ryan Maloney <rypmaloney@gmail.com>
2021-06-01 13:29:26 -0400

    Complete overhaul of functionality. Created game controller and turn alternator.

--- scripts.js
+++ scripts.js
@@ -1,105 +1,114 @@
-const container = document.getElementById('container');
+const boardController = (function () {
+
+    const container = document.getElementById('container');
+    const gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 
-
-//Gameboard 
-const gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-let playerArray = [[1, 2, 3],
-                   [4, 5, 6],
-                   [7, 8, 9],
-                   [1, 4, 7],
-                   [2, 5, 8],
-                   [3, 6, 9],
-                   [1, 5, 9],
-                   [3, 5, 7]]
+    function createBoard() {
+        resetBoard()
+        for (i = 0; i < gameBoard.length; i++) {
+            let gameSquare = document.createElement('div');
+            gameSquare.setAttribute('id', `${i + 1}`);
+            gameSquare.classList.add('box');
+            container.appendChild(gameSquare);
+        }
+    }
 
 
-//creates grid adds corresponding ID--index +1 
-function displayBoard() {
+    function updateBoard(player, position) {
+        let newBoard
+        let newMark = gameBoard.indexOf(position)
+        gameBoard.splice(newMark, 1, player)
 
-    resetBoard()
-
-    for (i = 0; i < gameBoard.length; i++) {
-        let gameSquare = document.createElement('div');
-        gameSquare.setAttribute('id', `${i + 1}`);
-        gameSquare.classList.add('box');
-        container.appendChild(gameSquare);
-
+        markPos = document.getElementById(`${position}`);
         let mark = document.createElement('p');
         mark.classList.add('mark');
-        gameSquare.appendChild(mark);
+        markPos.appendChild(mark);
+        mark.textContent = `${player.toUpperCase()}`
 
-        if (gameBoard[i] === 'x') {
-            mark.textContent = 'X'
+    }
 
-        } else if (gameBoard[i] === 'o') {
-            mark.textContent = 'O'
+    function resetBoard() {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
         }
     }
-}
+    createBoard()
 
-
-
-
-
-
-//resets gameboard -- end of game & to display board
-function resetBoard() {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
+    return {
+        createBoard,
+        resetBoard,
+        updateBoard
     }
-}
+})()
 
 
 
 
-//Player Factory
-const player = (mark) => {
-    const playerTurn = () => {
+const gameFlow = (function() {
+    let currentTurn = 'x';
+    let winner = '';
+    let playerArray = [[1, 2, 3],
+                       [4, 5, 6],
+                       [7, 8, 9],
+                       [1, 4, 7],
+                       [2, 5, 8],
+                       [3, 6, 9],
+                       [1, 5, 9],
+                       [3, 5, 7]];
+
+
+
+    function checkForWinner() {
+        let tieCount = 0
+        for (i = 0; i < playerArray.length; i++) {
+            if (playerArray[i][0] === 'x' &&
+                playerArray[i][1] === 'x' &&
+                playerArray[i][2] === 'x') {
+                console.log('winner is x')
+
+            } else if (playerArray[i][0] === 'o' &&
+                playerArray[i][1] === 'o' &&
+                playerArray[i][2] === 'o') {
+                console.log('winner is o')
+            }else if (playerArray[i].includes('x') && playerArray[i].includes('o')){
+                tieCount++
+            }
+        }
+        if (tieCount > 7) {console.log('tie')}
+    }
+
+
+
+    function changeTurn() {
+        currentTurn = currentTurn === 'x' ? 'o' : 'x';
+    }
+
+    function playerTurn() {
+        let markPosition;
         let boxes = document.querySelectorAll(".box");
-
         for (i = 0; i < boxes.length; i++) {
             boxes[i].addEventListener('click', (e) => {
-                //adds Eventlistener - adds player.mark to gameboard -> updates board
-                gameBoard[e.target.id - 1] = `${mark}`;
-                displayBoard()
-
-                //replaces all instances of turn position in the player array with player.mark
+                markPosition = parseInt(e.target.id);
+               
                 for (i = 0; i < playerArray.length; i++) {
-                    turnPos = playerArray[i].indexOf(parseInt(e.target.id));
-                    playerArray[i].splice(turnPos, 1, `${mark}`)
-
+                    if (playerArray[i].includes(markPosition)){
+                    let turnPos =  playerArray[i].indexOf(markPosition);
+                    playerArray[i].splice(turnPos, 1, `${currentTurn}`)}
                 }
-            })
+
+                boardController.updateBoard(currentTurn, markPosition)
+                checkForWinner()
+                changeTurn()
+            }, false)
         }
     }
-    return {
-        playerTurn,
-        mark,
-        playerArray
-    }
-}
+
+    return{playerTurn, playerArray, checkForWinner}
 
 
-//check for winner
-
-function checkForWinner() {
-    for (i = 0; i < playerArray.length; i++) {
-        if (playerArray[i][0] === 'x' &&
-            playerArray[i][1] === 'x' &&
-            playerArray[i][2] === 'x') {
-            console.log('playerx won!')
-        
-        } else if (playerArray[i][0] === 'o' && 
-                   playerArray[i][1] === 'o' &&
-                   playerArray[i][2] === 'o') {
-            console.log('playero won!')
-        }}
-    }
+})()
 
 
 
-
-displayBoard()
